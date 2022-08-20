ab7b17c055b6fef793d8ec2568f842f5d506a87d
Ander Sillva <andersillva11@gmail.com>
2022-08-20 14:47:06 -0300

    Criação do repositório.

---
+++ src/main/java/br/com/andersillva/gameflixcatalogoapi/GameflixCatalogoApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.andersillva.gameflixcatalogoapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GameflixCatalogoApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GameflixCatalogoApiApplication.class, args);
+	}
+
+}
