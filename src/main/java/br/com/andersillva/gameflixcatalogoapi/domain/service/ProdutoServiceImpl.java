66bab3574d719a3d6187853344066ab1f19b1a96
Ander Sillva <andersillva11@gmail.com>
2022-08-26 00:09:32 -0300

    Ajustes.

--- src/main/java/br/com/andersillva/gameflixcatalogoapi/domain/service/ProdutoServiceImpl.java
+++ src/main/java/br/com/andersillva/gameflixcatalogoapi/domain/service/ProdutoServiceImpl.java
@@ -7,6 +7,7 @@ import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.data.mongodb.core.query.Query;
 import org.springframework.data.mongodb.core.query.TextCriteria;
 import org.springframework.data.mongodb.core.query.TextQuery;
+import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -14,13 +15,14 @@ import br.com.andersillva.gameflixcatalogoapi.domain.document.Produto;
 import br.com.andersillva.gameflixcatalogoapi.domain.repository.ProdutoRepository;
 import br.com.andersillva.gameflixcatalogoapi.domain.service.exception.RegistroNaoEncontradoException;
 
+@Service
 public class ProdutoServiceImpl implements ProdutoService {
 
     @Autowired
     private MongoTemplate mongoTemplate;
-    
-    @Autowired
-    private ProdutoRepository produtoRepository;
+
+	@Autowired
+	private ProdutoRepository produtoRepository;
 
 	@Override
 	public Produto obterPorId(Long id) {
@@ -30,12 +32,12 @@ public class ProdutoServiceImpl implements ProdutoService {
 
 	@Override
 	public List<Produto> pesquisar(String termo) {
-        TextCriteria criteria = TextCriteria
-                .forDefaultLanguage()
-                .matchingPhrase(termo);
-
+	    TextCriteria criteria = TextCriteria
+	            .forDefaultLanguage()
+	            .matchingPhrase(termo);
+	
 		Query query = TextQuery.queryText(criteria).sortByScore();
-
+	
 		return mongoTemplate.find(query, Produto.class);
 	}
 
